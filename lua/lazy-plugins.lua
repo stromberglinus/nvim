e099539399c9c44120a3a9fa8f92ca95959320af
stromberglinus <Linus_stromberg@hotmail.com>
2024-08-09 17:14:41 +0200

    chore: Remove neo-tree

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -40,16 +40,15 @@ require("lazy").setup({
 	require("plugins/alpha"),
 
 	require("plugins/indent_blankline"),
-	
-  require("plugins/catppuccin"),
-  
-  require("plugins/zen_mode"),
+
+	require("plugins/catppuccin"),
+
+	require("plugins/zen_mode"),
 	-- require("plugins.noice.noice"),
 	-- require 'kickstart.plugins.debug',
 	-- require 'kickstart.plugins.indent_line',
 	-- require 'kickstart.plugins.lint',
 	-- require 'kickstart.plugins.autopairs',
-	-- require 'kickstart.plugins.neo-tree',
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
