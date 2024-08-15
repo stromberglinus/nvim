cdb40ee588db564d480fa0fc67e4676ab37ac734
stromberglinus <Linus_stromberg@hotmail.com>
2024-08-15 08:03:22 +0200

    chore: Update which key to v3

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,25 +1,24 @@
 return {
-    { -- Useful plugin to show you pending keybinds.
+    {
         "folke/which-key.nvim",
         event = "VimEnter", -- Sets the loading event to 'VimEnter'
-        opts = { triggers = {
-            { "s", mode = "nv" },
-            { "<auto>", mode = "nxsotv" },
-        } },
-        config = function(_,opts) -- This is the function that runs, AFTER loading
-            require("which-key").setup(opts)
-
-            -- Document existing key chains
-            require("which-key").add({
-                { "<leader>c", group = "Code" },
-                -- { "<leader>d", group = "Document" },
-                { "<leader>r", group = "Rename" },
+        opts = {
+            triggers = {
+                { "s", mode = "nv" },
+                { "<auto>", mode = "nxsotv" },
+            },
+            plugins = {
+                registers = true,
+            },
+            spec = {
+                { "<leader>c", group = "CMake" },
+                { "<leader>g", group = "Git" },
                 { "<leader>f", group = "Find" },
-                { "<leader>q", group = "Diagnostics" },
+                { "<leader>q", group = "Messages" },
+                { "<leader>r", group = "Refactor" },
                 { "<leader>t", group = "Toggle" },
                 { "<leader>x", group = "Trouble" },
-                { "<leader>g", group = "Git hunk", mode = { "n", "v" } },
-            })
-        end,
+            },
+        },
     },
 }
