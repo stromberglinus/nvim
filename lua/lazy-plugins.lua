f58c4c4c96fa8e4fbb1ead33ccecaebd71b1806a
stromberglinus <Linus_stromberg@hotmail.com>
2024-10-03 11:50:40 +0200

    feat: Add dap for debugging

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -53,6 +53,8 @@ require("lazy").setup({
     require("plugins.toggleterm"),
 
     require("plugins.grug"),
+
+    require("plugins.dap.init"),
 }, {
     ui = {
         -- If you are using a Nerd Font: set icons to an empty table which will use the
