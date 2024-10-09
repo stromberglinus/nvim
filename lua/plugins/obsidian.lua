972ace5ccb1cf5e36ed849c5e911a096d29e0158
stromberglinus <Linus_stromberg@hotmail.com>
2024-10-09 11:24:07 +0200

    feat: add obsidian

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,38 @@
+-- Try to load the vault_paths.lua file
+local ok, vault_paths = pcall(require, "vault_paths")
+
+-- If loading failed, set default paths
+if not ok then
+    vault_paths = {
+        personal = "~/workspace/ObsidianVaults/personal",
+        work = "~/workspace/ObsidianVaults/work",
+    }
+end
+
+return {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+        -- Required.
+        "nvim-lua/plenary.nvim",
+
+        -- Optional dependencies 👇
+        "hrsh7th/nvim-cmp",
+        "ibhagwan/fzf-lua",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+        workspaces = {
+            {
+                name = "personal",
+                path = vault_paths.personal,
+            },
+            {
+                name = "work",
+                path = vault_paths.work,
+            },
+        },
+    },
+}
