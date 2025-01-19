8221e369dd3cc3163e2883df484a7f0b4970ea54
linus stromberg <linus.stromberg@epiroc.com>
2025-01-19 13:27:11 +0100

    chore: set colors to render markdown

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,23 +2,99 @@ return {
     "MeanderingProgrammer/render-markdown.nvim",
     -- cmd = { "RenderMarkdown" },
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
+
+    init = function()
+        local color1_bg = "#b4befe"
+        local color2_bg = "#89b4fa"
+        local color3_bg = "#74c7ec"
+        local color4_bg = "#89dceb"
+        local color5_bg = "#94e2d5"
+        local color6_bg = "#cba6f7"
+        local color_fg = "#f5c2e7"
+
+        -- Define text (foreground) colors
+        local text_color1 = "#11111b" -- Red text for heading 1
+        local text_color2 = "#11111b" -- Green text for heading 2
+        local text_color3 = "#11111b" -- Blue text for heading 3
+        local text_color4 = "#11111b" -- Yellow text for heading 4
+        local text_color5 = "#11111b" -- Magenta text for heading 5
+        local text_color6 = "#11111b" -- Cyan text for heading 6
+
+        -- Background colors for headings
+        vim.cmd(string.format([[highlight Headline1Bg guifg=%s guibg=%s]], text_color1, color1_bg))
+        vim.cmd(string.format([[highlight Headline2Bg guifg=%s guibg=%s]], text_color2, color2_bg))
+        vim.cmd(string.format([[highlight Headline3Bg guifg=%s guibg=%s]], text_color3, color3_bg))
+        vim.cmd(string.format([[highlight Headline4Bg guifg=%s guibg=%s]], text_color4, color4_bg))
+        vim.cmd(string.format([[highlight Headline5Bg guifg=%s guibg=%s]], text_color5, color5_bg))
+        vim.cmd(string.format([[highlight Headline6Bg guifg=%s guibg=%s]], text_color6, color6_bg))
+
+        -- Bold text for icons or additional elements within headings
+        vim.cmd(string.format([[highlight Headline1Fg cterm=bold gui=bold guifg=%s]], text_color1))
+        vim.cmd(string.format([[highlight Headline2Fg cterm=bold gui=bold guifg=%s]], text_color2))
+        vim.cmd(string.format([[highlight Headline3Fg cterm=bold gui=bold guifg=%s]], text_color3))
+        vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], text_color4))
+        vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], text_color5))
+        vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], text_color6))
+
+        -- Add a custom color for emphasized text
+        local emphasis_color = "#8caaee" -- Change to your preferred color
+
+        -- Define emphasis text color
+        vim.cmd(string.format([[highlight markdownItalic guifg=%s]], emphasis_color))
+        vim.cmd(string.format([[highlight markdownBold guifg=%s]], emphasis_color))
+        vim.cmd(string.format([[highlight markdownBoldItalic guifg=%s]], emphasis_color))
+    end,
     opts = {
+
         heading = {
-            enabled = true,
-            sign = true,
+            sign = false,
             icons = { " ", " ", " ", " ", " ", " " },
             -- width = 'block',
-            backgrounds = {
-                "RenderMarkdownH4Bg",
-                "RenderMarkdownH5Bg",
-                "RenderMarkdownH6Bg",
-                "RenderMarkdownH1Bg",
-                "RenderMarkdownH2Bg",
-                "RenderMarkdownH3Bg",
+            bullet = {
+                -- Turn on / off list bullet rendering
+                enabled = true,
             },
-       },
+            checkbox = {
+                -- Turn on / off checkbox state rendering
+                enabled = true,
+                -- Determines how icons fill the available space:
+                --  inline:  underlying text is concealed resulting in a left aligned icon
+                --  overlay: result is left padded with spaces to hide any additional text
+                position = "inline",
+                unchecked = {
+                    -- Replaces '[ ]' of 'task_list_marker_unchecked'
+                    icon = "   󰄱 ",
+                    -- Highlight for the unchecked icon
+                    highlight = "RenderMarkdownUnchecked",
+                    -- Highlight for item associated with unchecked checkbox
+                    scope_highlight = nil,
+                },
+                checked = {
+                    -- Replaces '[x]' of 'task_list_marker_checked'
+                    icon = "   󰱒 ",
+                    -- Highlight for the checked icon
+                    highlight = "RenderMarkdownChecked",
+                    -- Highlight for item associated with checked checkbox
+                    scope_highlight = nil,
+                },
+            },
+            backgrounds = {
+                "Headline1Bg",
+                "Headline2Bg",
+                "Headline3Bg",
+                "Headline4Bg",
+                "Headline5Bg",
+                "Headline6Bg",
+            },
+            foregrounds = {
+                "Headline1Fg",
+                "Headline2Fg",
+                "Headline3Fg",
+                "Headline4Fg",
+                "Headline5Fg",
+                "Headline6Fg",
+            },
+        },
     },
     config = function(_, opts)
         require("obsidian").get_client().opts.ui.enable = false
