e3de9a5c4bce45a82b3b2d3d14176cd2609ebd78
linus stromberg <linus.stromberg@epiroc.com>
2025-01-28 11:32:45 +0100

    fix: fix conform format for typescript

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,7 +1,7 @@
 return {
     {
         "stevearc/conform.nvim",
-        event = { "BufWritePre" },
+        event = { "VeryLazy" }, -- Load early
         cmd = { "ConformInfo" },
         keys = {
             {
@@ -9,39 +9,69 @@ return {
                 function()
                     require("conform").format({ async = true, lsp_fallback = true })
                 end,
-                mode = "",
+                mode = { "n", "v" }, -- Normal and visual mode
                 desc = "Format",
             },
         },
         opts = {
             notify_on_error = false,
             format_on_save = function(bufnr)
-                -- Check if format on save is enabled using OPTIONS
-                if not OPTIONS.format_on_save.value then
-                    return false
+                -- Only format on save if user option is enabled
+                if OPTIONS and OPTIONS.format_on_save and OPTIONS.format_on_save.value then
+                    return {
+                        timeout_ms = 500,
+                        lsp_fallback = true,
+                    }
+                else
+                    return false -- Disable format on save
                 end
-
-                -- Disable format on save for certain filetypes
-                local disable_filetypes = { c = true, cpp = true }
-                return {
-                    timeout_ms = 500,
-                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-                }
             end,
             formatters_by_ft = {
                 ["_"] = { "trim_whitespace" },
                 lua = { "stylua" },
                 python = { "ruff_format" },
                 css = { "stylelint" },
-                javascript = { { "prettierd", "prettier" }, { "eslint_d", "eslint" } },
-                javascriptreact = {
-                    "stylelint",
-                    { "prettierd", "prettier" },
-                    { "eslint_d", "eslint" },
+                javascript = {
+                    "prettierd",
+                    "prettier",
+                    "eslint_d",
+                    "eslint",
+                    stop_after_first = true,
                 },
+                typescript = {
+                    "prettierd",
+                    "prettier",
+                    "eslint_d",
+                    "eslint",
+                    stop_after_first = true,
+                },
+                json = { "fixjson" },
                 markdown = { "markdownlint" },
                 cpp = { "clang-format" },
             },
         },
+        config = function(_, opts)
+            require("conform").setup(opts)
+
+            -- Set up keybinding manually
+            vim.keymap.set("n", "<leader>d", function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end, { desc = "Format" })
+
+            -- Ensure format-on-save still works
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*",
+                callback = function(args)
+                    local format_opts = opts.format_on_save(args.buf)
+                    if format_opts then
+                        require("conform").format({
+                            bufnr = args.buf,
+                            async = false,
+                            lsp_fallback = format_opts.lsp_fallback,
+                        })
+                    end
+                end,
+            })
+        end,
     },
 }
