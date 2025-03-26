6786f1280981ccb8cadba08cb674e78f61477035
linus stromberg <linus.stromberg@epiroc.com>
2025-03-26 11:25:31 +0100

    fix: clandD

--- lua/plugins/lsp/servers/clangd.lua
+++ lua/plugins/lsp/servers/clangd.lua
@@ -1,8 +1,3 @@
-local number_of_cores = 4
-if not _G.IS_WINDOWS or _G.IS_WSL then
-    number_of_cores = math.floor(tonumber(vim.fn.system("nproc")) * 0.5)
-end
-
 return {
     filetypes = {
         "c",
@@ -13,13 +8,13 @@ return {
     },
     cmd = {
         "clangd",
-        "-j=" .. tostring(number_of_cores),
+        "-j=4",
         "--background-index=true",
         "--clang-tidy",
         "--completion-style=detailed",
         "--malloc-trim",
         "--all-scopes-completion=true",
-        "--query-driver=" .. "/opt/sdks/**/*linux-g++",
+        "--query-driver=/opt/sdks/**/*linux-g++",
         "--header-insertion=iwyu",
     },
 }
--- lua/plugins/lsp/servers/init.lua
+++ lua/plugins/lsp/servers/init.lua
@@ -1,4 +1,12 @@
 return {
+    typos_lsp = {
+        init_options = { -- Sent to the LSP on init
+            diagnosticSeverity = "Hint",
+        },
+        filetypes = {
+            "*",
+        },
+    },
     -- azure_pipelines_ls = require("plugins.lsp.servers.azure_pipelines_ls"),
     basedpyright = require("plugins.lsp.servers.basedPyright"),
     clangd = require("plugins.lsp.servers.clangd"),
