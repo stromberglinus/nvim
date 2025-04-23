3ac8f2603c585536f9417896e0feea9415e798aa
linus stromberg <linus.stromberg@epiroc.com>
2025-04-23 09:44:07 +0200

    chore: remove line numbers

--- lua/settings.lua
+++ lua/settings.lua
@@ -6,9 +6,10 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
+vim.g.enable_copilot = true
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.number = false
+vim.opt.relativenumber = false
 vim.opt.mouse = "a"
 vim.opt.showmode = false
 
