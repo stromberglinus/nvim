6ff45af2b920e72ef9ef792956e1fea8febcab5b
linus stromberg <linus.stromberg@epiroc.com>
2025-04-23 09:43:13 +0200

    feat: enable copilot

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -57,6 +57,8 @@ require("lazy").setup({
     require("plugins.cmake"),
 
     require("plugins.dressing"),
+
+    require("plugins.copilot"),
 }, {
     ui = {
         -- If you are using a Nerd Font: set icons to an empty table which will use the
